7d2c08b28de2a0fc0c7b1caef604397e957c1491
Precious <preciousmadubuike@preciouss-MacBook-Pro.local>
2020-07-22 22:56:54 +0200

    Update main.js

--- main.js
+++ main.js
@@ -286,7 +286,63 @@ let quotes = [
 
     '"If you need a degree to do it, it’s not going to make you wealthy."',
 
-    '"In the startup olympics, first place gets a monopoly, second place gets a medal, and there is no third place."'
+    '"In the startup olympics, first place gets a monopoly, second place gets a medal, and there is no third place."',
+
+    '"Seek wealth, not money or status. Wealth is having assets that earn while you sleep. Money is how we transfer time and wealth. Status is your place in the social hierarchy."',
+
+// new
+
+    '"Understand that ethical wealth creation is possible. If you secretly despise wealth, it will elude you."',
+
+    '"Ignore people playing status games. They gain status by attacking people playing wealth creation games."',
+
+    '"You’re not going to get rich renting out your time. You must own equity - a piece of a business - to gain your financial freedom."',
+
+    '"You will get rich by giving society what it wants but does not yet know how to get. At scale."',
+
+    '"Pick an industry where you can play long term games with long term people."',
+
+    '"The Internet has massively broadened the possible space of careers. Most people have not figured this out yet."',
+
+    '"Play iterated games. All the returns in life, whether in wealth, relationships, or knowledge, come from compound interest."',
+
+    '"Pick business partners with high intelligence, energy, and, above all, integrity."',
+
+    "'Don't partner with cynics and pessimists. Their beliefs are self-fulfilling.'",
+
+    '"Learn to sell. Learn to build. If you can do both, you will be unstoppable."',
+
+    '"Arm yourself with specific knowledge, accountability, and leverage."',
+
+    '"Specific knowledge is knowledge that you cannot be trained for. If society can train you, it can train someone else, and replace you."',
+
+    '"Specific knowledge is found by pursuing your genuine curiosity and passion rather than whatever is hot right now."',
+
+    '"Specific knowledge is often highly technical or creative. It cannot be outsourced or automated."',
+
+    '"Embrace accountability, and take business risks under your own name. Society will reward you with responsibility, equity, and leverage."',
+
+    '"The most accountable people have singular, public, and risky brands: Oprah, Trump, Kanye, Elon."',
+
+    "'Labor means people working for you. It's the oldest and most fought-over form of leverage. Labor leverage will impress your parents, but don’t waste your life chasing it.'",
+
+    "'Code and media are permissionless leverage. They're the leverage behind the newly rich. You can create software and media that works for you while you sleep.'",
+
+    "'An army of robots is freely available - it's just packed in data centers for heat and space efficiency. Use it.'",
+
+    "'If you can't code, write books and blogs, record videos and podcasts.'",
+
+    "'There is no skill called “business.” Avoid business magazines and business classes.'",
+
+    '"Study microeconomics, game theory, psychology, persuasion, ethics, mathematics, and computers."',
+
+    '"Reading is faster than listening. Doing is faster than watching."',
+
+    "'Set and enforce an aspirational personal hourly rate. If fixing a problem will save less than your hourly rate, ignore it. If outsourcing a task will cost less than your hourly rate, outsource it.'"
+
+
+
+
     
 
 
